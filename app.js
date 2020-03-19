b45e077ddae8bd431798a9c95e71a3ede8c6b584
ryan <ryanmaulanaputra@gmail.com>
2020-03-19 19:20:19 +0700

    added payload receiver from client and win condition checker

--- app.js
+++ app.js
@@ -7,16 +7,70 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3211;
 
-app.get('/', function (req, res) {
-    res.send('Hello njing!');
-});
+let board = [
+	[ '', '', '' ],
+	[ '', '', '' ],
+	[ '', '', '' ]
+]
+
+let defaultBoard = [
+	[ '', '', '' ],
+	[ '', '', '' ],
+	[ '', '', '' ]
+]
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    socket.on('mark', function (payload) {
+        const { x, y, value } = payload
+        board[x][y] = value
+
+        // check win logic
+        let strDiagLeft = board[0][0] + board[1][1] + board[2][2]
+        let strDiagRight = board[0][2] + board[1][1] + board[2][0]
+        let winner = ''
+
+        for (let i = 0; i < board.length; i++) {
+            let strH = ''
+            let strV = ''
+            for (let j = 0; j < board.length; j++) {
+                strH += board[i][j]
+            }
+
+            for (let j = 0; j < board.length; j++) {
+                strV += board[j][i]
+            }
+
+            if (strH === 'xxx' || strV === 'xxx' || strDiagLeft === 'xxx' ||  strDiagRight === 'xxx') {
+                // display player X is win & display player O is lose
+                // console.log(`Player X is the winner!`);
+                winner = 'X'
+                break;
+            } else if (strH === 'ooo' || strV === 'ooo' || strDiagLeft === 'ooo' || strDiagRight === 'ooo') {
+                // display player O is win & display player X is lose
+                console.log(`Player O is the winner!`);
+                winner = 'O'
+                break;
+            }
+        }
+
+        let payload = {
+            board,
+            winner
+        }
+
+        io.emit('update-board', payload)
+    })
+
+    socket.on('reset-board', function () {
+        io.emit('update-board', defaultBoard)
+    })
+
+    // console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
 });
+
 
 http.listen(PORT, function () {
     console.log('listening on PORT: ', PORT);
